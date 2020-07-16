f03d75135b4895d1f015814dbb768f3c57243969
André Nogueira <andre.nogueira@epfl.ch>
2020-07-16 15:21:50 +0200

    fix: chain router using .use() and not .get()

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const analyticsRouter = require('./routers/analytics');
 const tasksRouter = require('./routers/tasks');
 
-router.get('/analytics', analyticsRouter);
-router.get('/tasks', tasksRouter);
+router.use('/analytics', analyticsRouter);
+router.use('/tasks', tasksRouter);
 
 module.exports = router;
